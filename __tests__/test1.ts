264b48e43d3a157d3caa39154b3c13232feaeca8
niris <nirisix@gmail.com>
2017-12-29 20:16:34 +0800

    update: add test

--- __tests__/test1.ts
+++ __tests__/test1.ts
@@ -1,10 +1,11 @@
 import cron from "../src";
 
-jest.setTimeout(70 * 1000);
+jest.setTimeout(3 * 60 * 1000);
 
-test("crontab", done => {
+test("every minute", done => {
     cron("* * * * *").subscribe(_ => {
         const date = new Date();
+        console.log(date);
         expect(date.getSeconds()).toBe(0);
         done();
     });
