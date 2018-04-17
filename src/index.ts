58867e1d552dc3dc33b284df9320f1dc6a901ad8
niris <nirisix@gmail.com>
2018-04-17 11:04:10 +0800

    chore

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import parse from "./parser";
 import timer from "./timer";
 
 import { Observable, Tdate } from "./_type";
-export { Observable, Tdate };
 
 const cron = (cronExpr: string): Observable<Tdate> => {
     const pred = convert(parse(cronExpr));
